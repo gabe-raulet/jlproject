33e3d29a6afbb5cfa86567286d848b49c4e6fb1a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-11-07 14:01:08 -0800

    checkpoint

--- create_groundtruth.c
+++ create_groundtruth.c
@@ -8,9 +8,17 @@
 int usage(char const *prg);
 int read_fvecs(char const *fname, float **vecs, int *d);
 
+typedef struct
+{
+    int id;
+    float dist;
+} item_t;
+
+int item_cmp(const void *v1, const void *v2);
+
 int main(int argc, char *argv[])
 {
-    int i;
+    int i, j;
     char *base_fname, *query_fname, *truth_fname;
 
     if (argc != 7) return usage(argv[0]);
@@ -29,16 +37,45 @@ int main(int argc, char *argv[])
     }
 
     float *base_vecs, *query_vecs;
-    int base_num, query_num, base_dim, query_dim;
+    int base_num, query_num, base_dim, query_dim, d;
 
     base_num = read_fvecs(base_fname, &base_vecs, &base_dim);
     query_num = read_fvecs(query_fname, &query_vecs, &query_dim);
 
-    printf("num base vectors = %d\n", base_num);
-    printf("num query vectors = %d\n", query_num);
+    assert(query_dim == base_dim);
+    d = query_dim;
+
+    item_t *items = calloc(base_num, sizeof(item_t));
+
+    for (i = 0; i < query_num; ++i)
+    {
+        float *q = &query_vecs[i*d];
+        double dist = 0.0;
+
+        for (j = 0; j < base_num; ++j)
+        {
+            float *p = &base_vecs[i*d];
+
+            for (int k = 0; k < d; ++k)
+            {
+                dist += (p[k] - q[k]) * (p[k] - q[k]);
+            }
+
+            items[j].id = j;
+            items[j].dist = (float)dist;
+        }
+
+        qsort(items, (size_t)base_num, sizeof(item_t), item_cmp);
+
+        for (j = 0; j < 100; ++j)
+        {
+            printf("%d\t%f\n", items[j].id, sqrt(items[j].dist));
+        }
+    }
 
     free(base_vecs);
     free(query_vecs);
+    free(items);
 
     return 0;
 }
@@ -78,4 +115,18 @@ int read_fvecs(char const *fname, float **vecs, int *d)
 
     return n;
 }
+
+int item_cmp(const void *v1, const void *v2)
+{
+    const item_t *item1 = (item_t*)v1;
+    const item_t *item2 = (item_t*)v2;
+
+    if (item1->dist != item2->dist)
+    {
+        return item1->dist < item2->dist? -1 : 1;
+    }
+
+    return 0;
+}
+
 
