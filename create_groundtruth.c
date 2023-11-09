2a2cc65fc9efc020d87925b56a02ab42064f1626
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-11-09 14:00:08 -0800

    compiles but error

--- create_groundtruth.c
+++ create_groundtruth.c
@@ -5,15 +5,19 @@
 #include <math.h>
 #include "vecs_io.h"
 
+typedef struct { int id; float dist; } tuple_t;
+
 int usage(char const *prg);
 int read_fvecs(char const *fname, float **vecs, int *d);
+int knns(void *mem, int *ids, const float *qvec, const float *base, int k, int n, int d);
+int tuple_compare(const void *v1, const void *v2);
 
 int main(int argc, char *argv[])
 {
     int i, j, k;
     char *base_fname, *query_fname, *truth_fname;
 
-    if (argc != 8) return usage(argv[0]);
+    if (argc != 9) return usage(argv[0]);
 
     for (i = 1; i < argc; ++i)
     {
@@ -38,8 +42,29 @@ int main(int argc, char *argv[])
     assert(query_dim == base_dim);
     d = query_dim;
 
+    FILE *fp;
+    void *mem;
+    int *ids;
+    float const *qvec = NULL;
+
+    mem = malloc(base_num * sizeof(tuple_t));
+    ids = malloc(k * sizeof(int));
+    fp = fopen(truth_fname, "wb");
+
+    for (int qid = 0; qid < query_num; ++qid)
+    {
+        qvec = &query_vecs[qid * d];
+        knns(mem, ids, qvec, base_vecs, k, base_num, d);
+
+        fwrite(&k, sizeof(int), 1, fp);
+        fwrite(ids, sizeof(int), k, fp);
+    }
+
+    fclose(fp);
     free(base_vecs);
     free(query_vecs);
+    free(mem);
+    free(ids);
 
     return 0;
 }
@@ -79,4 +104,40 @@ int read_fvecs(char const *fname, float **vecs, int *d)
     *vecs = v;
 
     return n;
+}
+
+int knns(void *mem, int *ids, const float *qvec, const float *base, int k, int n, int d)
+{
+    tuple_t *tuples = (tuple_t *)mem;
+
+    for (int pid = 0; pid < n; ++pid)
+    {
+        double dist = 0.;
+        float const *bvec = &base[pid*d];
+
+        for (int i = 0; i < d; ++i)
+        {
+            double delta = (qvec[i] - bvec[i]);
+            dist += delta * delta;
+        }
+
+        tuples[pid].id = pid;
+        tuples[pid].dist = dist;
+    }
+
+    qsort(tuples, n, sizeof(tuple_t), tuple_compare);
+
+    for (int i = 0; i < k; ++i)
+        ids[i] = tuples[i].id;
+
+    return 0;
+}
+
+int tuple_compare(const void *v1, const void *v2)
+{
+    const tuple_t *t1 = (const void *)v1;
+    const tuple_t *t2 = (const void *)v2;
+
+    if (t1->dist != t2->dist) return t1->dist < t2->dist? -1 : 1;
+    else return 0;
 }
